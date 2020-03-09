8e80b3649341099efd5dba79972fb041448c2b41
class02 <class02@DESKTOP-PCQNQQF>
2020-03-09 15:06:13 +0900

    214

---
+++ java00/src/day01/Test01.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class Test01 {
+	public static void main(String [] args) {
+		System.out.println("Hello Lava!!!");
+		// ==> println() 함수(Method) - 콘솔창에 내용을 출력해주는 기능을 가진 함수.
+		// 이클립스에서 자바클래스 실행하기
+		// Ctrl + Shift + F11
+		
+	}
+}
